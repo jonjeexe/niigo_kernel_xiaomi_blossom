22b95b71532d8a8223b6d72f3d34c2368eb17c25
Jonje <168655485+jonjeexe@users.noreply.github.com>
2026-07-07 11:15:06 +0530

    gpu: dvfs: increase GPU DVFS aggressiveness for gaming

--- drivers/gpu/mediatek/ged/src/ged_dvfs.c
+++ drivers/gpu/mediatek/ged/src/ged_dvfs.c
@@ -37,14 +37,14 @@
 #include "ged_global.h"
 
 #define MTK_DEFER_DVFS_WORK_MS          10000
-#define MTK_DVFS_SWITCH_INTERVAL_MS     50
+#define MTK_DVFS_SWITCH_INTERVAL_MS     16
 
 /* Definition of GED_DVFS_SKIP_ROUNDS is to skip DVFS when boost raised
  *  the value stands for counting down rounds of DVFS period
  *  Current using vsync that would be 16ms as period,
  *  below boost at (32, 48] seconds per boost
  */
-#define GED_DVFS_SKIP_ROUNDS 3
+#define GED_DVFS_SKIP_ROUNDS 6
 
 #ifdef GED_ENABLE_FB_DVFS
 spinlock_t gsGpuUtilLock;
@@ -177,7 +177,7 @@ struct GpuUtilization_Ex g_Util_Ex;
 static int ged_get_dvfs_loading_mode(void);
 #endif
 
-#define GED_DVFS_TIMER_BASED_DVFS_MARGIN 30
+#define GED_DVFS_TIMER_BASED_DVFS_MARGIN 60
 static int gx_tb_dvfs_margin = GED_DVFS_TIMER_BASED_DVFS_MARGIN;
 static int gx_tb_dvfs_margin_cur = GED_DVFS_TIMER_BASED_DVFS_MARGIN;
 #ifdef GED_ENABLE_TIMER_BASED_DVFS_MARGIN
