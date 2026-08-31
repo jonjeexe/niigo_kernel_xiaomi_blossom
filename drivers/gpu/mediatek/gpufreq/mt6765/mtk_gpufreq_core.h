615d8e43ee76851cbc20afaa06b037fe403bef64
Jonje <168655485+jonjeexe@users.noreply.github.com>
2026-08-31 13:09:21 +0530

    Overclock GPU DVFS frequency and voltage settings of segment_2
    Overclock 650mhz to 730mhz

--- drivers/gpu/mediatek/gpufreq/mt6765/mtk_gpufreq_core.h
+++ drivers/gpu/mediatek/gpufreq/mt6765/mtk_gpufreq_core.h
@@ -18,17 +18,17 @@
 /**************************************************
  * MT6762 segment_2 : GPU DVFS OPP table Setting
  **************************************************/
-#define SEG2_GPU_DVFS_FREQ0			(650000)/* KHz */
-#define SEG2_GPU_DVFS_FREQ1			(500000)/* KHz */
-#define SEG2_GPU_DVFS_FREQ2			(400000)/* KHz */
+#define SEG2_GPU_DVFS_FREQ0			(730000) /* KHz */
+#define SEG2_GPU_DVFS_FREQ1			(500000) /* KHz */
+#define SEG2_GPU_DVFS_FREQ2			(400000) /* KHz */
 
-#define SEG2_GPU_DVFS_VOLT0			SEG3_GPU_DVFS_VOLT0
-#define SEG2_GPU_DVFS_VOLT1			SEG3_GPU_DVFS_VOLT1
-#define SEG2_GPU_DVFS_VOLT2			SEG3_GPU_DVFS_VOLT2
+#define SEG2_GPU_DVFS_VOLT0			(80000) /* mV x 100 */
+#define SEG2_GPU_DVFS_VOLT1			(70000) /* mV x 100 */
+#define SEG2_GPU_DVFS_VOLT2			(65000) /* mV x 100 */
 
-#define SEG2_GPU_DVFS_VSRAM0			SEG3_GPU_DVFS_VSRAM0
-#define SEG2_GPU_DVFS_VSRAM1			SEG3_GPU_DVFS_VSRAM1
-#define SEG2_GPU_DVFS_VSRAM2			SEG3_GPU_DVFS_VSRAM2
+#define SEG2_GPU_DVFS_VSRAM0	    (87500) /* mV x 100 */
+#define SEG2_GPU_DVFS_VSRAM1		(87500) /* mV x 100 */
+#define SEG2_GPU_DVFS_VSRAM2		(87500) /* mV x 100 */
 
 /**************************************************
  * MT6765 segment_3 : GPU DVFS OPP table Setting
